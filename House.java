38618b1771c74bd3cd4ecb1b0c9e8a245d4aa8e3
Joanne Hoar <joanne.hoar@gmail.com>
2025-06-14 21:18:45 -0600

    comment added, code cleanup

--- House.java
+++ House.java
@@ -1,12 +1,30 @@
+/**
+ * Represents a house with a floor area and sale price.
+ * Provides methods to build the house and calculate its sale price.
+ *
+ * Author: Joanne Hoar
+ */
 public class House {
+    // The floor area of the house in square units
     private double floorArea;
+    // The sale price of the house
     private double salePrice;
 
+    /**
+     * Builds the house by setting its floor area.
+     * Prints the floor area to the console.
+     *
+     * @param floorArea the floor area of the house
+     */
     public void build(double floorArea) {
         this.floorArea = floorArea;
         System.out.println(floorArea);
     }
 
+    /**
+     * Calculates and sets the sale price of the house based on its floor area.
+     * Prints the sale price to the console.
+     */
     public void sell() {
         this.salePrice = 150 * this.floorArea;
         System.out.println(this.salePrice);
--- Housing.java
+++ Housing.java
@@ -1,22 +1,36 @@
+/**
+ * Demonstrates building and selling multiple houses.
+ * Generates random floor areas for each house, builds and sells them,
+ * and prints the total floor area.
+ *
+ * Author: Joanne Hoar
+ */
 import java.util.Random;
 
 public class Housing {
+    /**
+     * Main method to build and sell 3 houses with random floor areas.
+     * Prints the floor area and sale price for each house,
+     * and the total floor area at the end.
+     *
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
-        House house;
-        double floorArea;
-        double totalFloorArea = 0;
-        Random rand = new Random();
-
+        double totalFloorArea = 0.0;
+        Random random = new Random();
         System.out.println("Building and selling 3 houses:");
-        for (int i = 0; i < 3; i++) {
-            floorArea = 1000 + (rand.nextDouble() * (2500 - 1000));
-            house = new House();
+
+        for (int i = 0; i < 3; ++i) {
+            // Generate a random floor area between 1000.0 and 2500.0
+            double floorArea = 1000.0 + random.nextDouble() * 1500.0;
+            House house = new House();
             System.out.print("House " + (i + 1) + " floor area: ");
             house.build(floorArea);
             totalFloorArea += floorArea;
             System.out.print("House " + (i + 1) + " sale price: ");
             house.sell();
         }
+
         System.out.println("Total floor area of all houses: " + totalFloorArea);
     }
 }
